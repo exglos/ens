4b5639e88a333ffff7303f180624e29e0b35191a
naftalimurgor <murgornaftali@gmail.com>
2023-05-20 13:26:43 +0300

    Add Resolver contract

---
+++ contracts/Resolver.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.5.0;
+
+/**
+ * @title Resolver
+ * @author exglos.eth
+ * @notice A basic interface for ENS resolver
+ */
+contract Resolver {
+  function supportsInterface(bytes4 intefaceId) public pure returns(bool);
+  function addr(bytes node) public view returns (address);
+  function setAddr(bytes32 node, addr address) public;
+}
