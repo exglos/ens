c494538c2b96be951bfce54c49cfead482306fe9
naftalimurgor <murgornaftali@gmail.com>
2023-05-20 14:23:55 +0300

    add RegistrarInterface

---
+++ contracts/RegistrarInterface.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.5.0;
+
+interface RegistrarInterface {
+  event DomainOwnerChanged(bytes32 indexed, address indexed oldDomainOwner, address indexed newDomainOwner);
+  event DomainConfigured(bytes32 indexed label);
+  event DomainUnlisted(bytes32 indexed label);
+  event NewDomainRegistration(bytes32 indexed label, string subdomain, address indexed owner, address indexed referrer, uint price);
+  event DomainRentPaid(bytes32 indexed label, string subdomain, uint amount, uint expirationDate);
+
+  // interfaceID of these 4 method is 0xc1b15f5a
+  function query(bytes32 label, string calldata subdomain) external view returns (string memory domain, uint signupFee, uint rent, uint referralPPM);
+  function register(bytes32 label, string calldata subdomain, address owner, address payable referrer);
+  
+  function rentDue(bytes32 label, string calldata subdomain) external view returns (uint timestamp);
+  function payRent(bytes32 label, string calldata subdomain) external payable;
+}
