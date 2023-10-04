64dc8de69eb62ed2230b799de039c08c45dacecc
naftalimurgor <murgornaftali@gmail.com>
2023-10-04 15:42:00 +0300

    add sign up route

--- exglos.ens/src/App.jsx
+++ exglos.ens/src/App.jsx
@@ -4,6 +4,7 @@ import Claim from './Checkout';
 import Success from './Success';
 import Admin from './Admin';
 import AdminLogin from './AdminLogin';
+import AdminSignup from './AdminSignup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path='/admin' element={<Admin/>} />
         <Route path='/login' element={<AdminLogin/>} />
+        <Route path='/join' element={<AdminSignup />} />
         <Route path='/claim/:subdomain' element ={<Claim/>}/>
         <Route path='/success/:txHash' element ={<Success/>}/>
 
