2bc22099c95990faf0239015e77f977a4d53e286
naftalimurgor <murgornaftali@gmail.com>
2023-10-10 18:43:06 +0300

    add Firebase config

---
+++ exglos.ens/src/config/firebase-config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaXX-HPbOZJo-byTH9WEaCsEXPnd8WUf0",
+    authDomain: "exglos-api.firebaseapp.com",
+    projectId: "exglos-api",
+    storageBucket: "exglos-api.appspot.com",
+    messagingSenderId: "153049980031",
+    appId: "1:153049980031:web:b97e935c40942c05d6f34a",
+    measurementId: "G-YTKXRQX0PT"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig)
