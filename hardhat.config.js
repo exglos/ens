61f4d4cbc7bf1e993a26a36863f99c582c0a4e58
naftalimurgor <murgornaftali@gmail.com>
2023-10-19 14:17:35 +0300

    update `hardhat.config.js`

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,16 +4,22 @@ const dotenv = require('dotenv')
 dotenv.config() // make all environment secrets available
 
 const PRIVATE_KEY1 = process.env.PRIVATE_KEY
+const PRIVATE_KEY2 = process.env.PRIVATE_KEY2
+
 console.log(PRIVATE_KEY1)
 
 const config = {
   // defaultNetwork: "rinkeby",
   networks: {
     hardhat: {},
-    // rinkeby: {
-    //   url: 'https://rinkeby.infura.io/v3/45d19d0de4b5421da4fec7e90dff071d',
-    //   accounts: [PRIVATE_KEY1]
-    // }
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/04c3f6b3201b41ef8a15e7e08a7c730e',
+      accounts: [PRIVATE_KEY1]
+    },
+    mainnet: {
+      url: 'https://mainnet.infura.io/v3/04c3f6b3201b41ef8a15e7e08a7c730e',
+      accounts: [PRIVATE_KEY2]
+    }
   },
   solidity: {
     compilers: [
