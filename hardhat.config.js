c3981c5c99d9720fd481918714330ef4117997a3
naftalimurgor <murgornaftali@gmail.com>
2023-11-13 10:26:01 +0300

    config: update `hardhat.config` with Sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,17 +9,16 @@ const PRIVATE_KEY2 = process.env.PRIVATE_KEY2
 console.log(PRIVATE_KEY1)
 
 const config = {
-  // defaultNetwork: "rinkeby",
   networks: {
     hardhat: {},
     sepolia: {
       url: 'https://sepolia.infura.io/v3/04c3f6b3201b41ef8a15e7e08a7c730e',
       accounts: [PRIVATE_KEY1]
     },
-    mainnet: {
-      url: 'https://mainnet.infura.io/v3/04c3f6b3201b41ef8a15e7e08a7c730e',
-      accounts: [PRIVATE_KEY2]
-    }
+    // mainnet: {
+    //   url: 'https://mainnet.infura.io/v3/04c3f6b3201b41ef8a15e7e08a7c730e',
+    //   accounts: [PRIVATE_KEY2]
+    // }
   },
   solidity: {
     compilers: [
