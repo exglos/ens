9ea5ce7c731c899eb0a9c15201a952e9bcbea748
naftalimurgor <murgornaftali@gmail.com>
2023-11-13 09:59:43 +0300

    refactor: no need of Login logic wallet shuld handle that

--- exglos.ens/src/App.jsx
+++ exglos.ens/src/App.jsx
@@ -1,21 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Claim from './Checkout';
-import Success from './Success';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Admin from './Admin';
 import AdminLogin from './AdminLogin';
 import AdminSignup from './AdminSignup';
+import Claim from './Checkout';
+import Home from './Home';
+import UserLogin from './Login';
+import Success from './Success';
+import './config/firebase-config';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/admin' element={<Admin/>} />
-        <Route path='/login' element={<AdminLogin/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }/>
+        {/* <Route path='/user/login' element={<UserLogin />} /> */}
+        {/* <Route path='/admin/login' element={<AdminLogin />} /> */}
         <Route path='/join' element={<AdminSignup />} />
-        <Route path='/claim/:subdomain' element ={<Claim/>}/>
-        <Route path='/success/:txHash' element ={<Success/>}/>
+        <Route path='/claim/:subdomain' element={<Claim />} />
+        <Route path='/success/:txHash' element={<Success />} />
 
       </Routes>
     </BrowserRouter>
--- exglos.ens/src/components/Nav.jsx
+++ exglos.ens/src/components/Nav.jsx
@@ -8,96 +8,93 @@ const ONBOARD_TEXT = 'Click here to install MetaMask!'
 const CONNECT_WALLET = 'Connect Metamask Wallet'
 
 export function Nav() {
-    // onboarding button text
-    const [buttonText, setButtonText] = React.useState('')
+  // onboarding button text
+  const [buttonText, setButtonText] = React.useState('')
 
-    const onboarding = React.useRef()
-    const [accounts, setAccounts] = React.useState([])
+  const onboarding = React.useRef()
+  const [accounts, setAccounts] = React.useState([])
 
-    const [account, setCurrentAccount] = React.useState('')
-    const [signer, setSigner] = React.useState('')
+  const [account, setCurrentAccount] = React.useState('')
+  const [signer, setSigner] = React.useState('')
 
-    useEffect(() => {
-        if (!onboarding.current) {
-            // create an instance fo MetamaskOnboarding class when component mounts for the first time
-            onboarding.current = new MetaMaskOnboarding()
-        }
-    }, [])
-
-    useEffect(() => {
-        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-            setButtonText(CONNECT_WALLET)
-            onboarding.current?.stopOnboarding()
-        } else {
-            setButtonText(ONBOARD_TEXT)
-        }
-    }, [])
-
-    // connect initialize onboarding or connect wallet
-    const connectWallet = async () => {
-        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-            try {
-
-                const [_signer, provider] = await getSigner()
-                const address = await _signer.getAddress()
-                setSigner(_signer)
-                setButtonText(address)
-                setCurrentAccount(address)
-            } catch (e) {
-                alert(e.message)
-                console.log(e)
-            }
-        } else {
-            // opens a new tab to the <chrome | firefox> store for user to install the MetaMask browser extension
-            onboarding.current?.startOnboarding()
-        }
+  useEffect(() => {
+    if (!onboarding.current) {
+      // create an instance fo MetamaskOnboarding class when component mounts for the first time
+      onboarding.current = new MetaMaskOnboarding()
     }
+  }, [])
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    Exglos ENS
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#pricing">
-                                Pricing
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/login">
-                                Login
-                            </a>
-                        </li>
-                        <li className="nav-item">
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      setButtonText(CONNECT_WALLET)
+      onboarding.current?.stopOnboarding()
+    } else {
+      setButtonText(ONBOARD_TEXT)
+    }
+  }, [])
+
+  // connect initialize onboarding or connect wallet
+  const connectWallet = async () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      try {
+
+        const [_signer, provider] = await getSigner()
+        const address = await _signer.getAddress()
+        setSigner(_signer)
+        setButtonText(address)
+        setCurrentAccount(address)
+      } catch (e) {
+        alert(e.message)
+        console.log(e)
+      }
+    } else {
+      // opens a new tab to the <chrome | firefox> store for user to install the MetaMask browser extension
+      onboarding.current?.startOnboarding()
+    }
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Exglos ENS
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/#pricing">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+            </li>
+            {/* <li className="nav-item">
                             <a className="nav-link" href="/join">
                                 Sign Up
                             </a>
-                        </li>
+                        </li> */}
 
 
-                    </ul>
-                </div>
-                <span className="badge rounded-pill text-bg-primary">{accounts[0]}</span>
-                <button type="button"
-                    onClick={connectWallet}
-                    className="btn btn-outline-primary">
-                    {buttonText}
-                </button>
-            </div>
-        </nav>
-    )
+          </ul>
+        </div>
+        <span className="badge rounded-pill text-bg-primary">{accounts[0]}</span>
+        <button type="button"
+          onClick={connectWallet}
+          className="btn btn-outline-primary">
+          {buttonText}
+        </button>
+      </div>
+    </nav>
+  )
 }
